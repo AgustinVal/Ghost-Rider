8f08cd566b33b85e3daf604049bc30a3f3cc6a65
Bastian-Reyes <bastianreyes@ug.uchile.cl>
2022-05-30 11:22:16 -0400

    Se incluye un enemigo nuevo

---
+++ Ghost_Rider/KinematicBody2D.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+
+var speed =10
+var acceleration= 200
+
+var velocity=Vector2()
+
+func _ready():
+	pass # Replace with function body.
+	
+func _physics_process(delta):
+	pass#velocity= move_and_slide(velocity, Vector2.)
+
+func se_asusta(instigator: Node2D):
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
