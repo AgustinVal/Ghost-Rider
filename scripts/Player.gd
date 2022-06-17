ca74b0ece33d9e944ea5c2189458147c4e2359fa
Bastian-Reyes <bastianreyes@ug.uchile.cl>
2022-06-17 12:52:51 -0400

    Giro Horizontal

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,31 +3,47 @@ extends KinematicBody2D
 onready var hurtbox = $Hurtbox
 onready var ray_cast = $RayCast2D
 onready var anim_tree= $AnimationTree
-onready var scare_spawn = $ScareSpawn
+onready var scare_spawn = $Pivot/ScareSpawn
+onready var pivot = $Pivot
 onready var playback = anim_tree.get("parameters/playback")
-
+var velocity = Vector2()
 var linear_vel = Vector2.ZERO 
 var SPEED = 350
+var ACCELERATION =100
 var health = 100
+
+export var scaring = false
 var SoundWave = preload("res://scenes/SoundWave.tscn")
 
 	
 func _ready():
+	scaring = false
 	anim_tree.active=true
 
 
 func _physics_process(_delta):
-	var target_velY = Input.get_action_strength("down") -   Input.get_action_strength("up")
-	var target_velX = Input.get_action_strength("right") -  Input.get_action_strength("left")
-	linear_vel.y = lerp(linear_vel.y, target_velY * SPEED, 0.5)
-	linear_vel.x = lerp(linear_vel.x, target_velX * SPEED, 0.5)
-	linear_vel = move_and_slide(linear_vel)
+	velocity = move_and_slide(velocity, Vector2.UP)
+	var move_input_horizontal = Input.get_axis("left", "right")
+	var move_input_vertical = Input.get_axis("up", "down")
+	
+	velocity.x = move_toward(velocity.x, move_input_horizontal * SPEED, ACCELERATION)
+	velocity.y = move_toward(velocity.y, move_input_vertical * SPEED, ACCELERATION)
+	
+	if scaring:
+		move_input_vertical = 0 
+		move_input_horizontal = 0
 	
 	if Input.is_action_just_pressed("interact") and ray_cast.is_colliding():
 		var collider = ray_cast.get_collider()
 		if collider.has_method("interact"):
 			collider.interact()
 			print("is activated: ",collider.is_activated())
+	
+	if Input.is_action_pressed("right") and not Input.is_action_just_pressed("left"):
+		pivot.scale.x = 1
+	if Input.is_action_pressed("left") and not Input.is_action_just_pressed("right"):
+		pivot.scale.x = -1
+	
 	
 	if  Input.is_action_just_pressed("Boo"):
 		Scare()
@@ -36,6 +52,8 @@ func Scare():
 	var wave =SoundWave.instance()
 	get_parent().add_child(wave)
 	wave.global_position = scare_spawn.global_position
+	if pivot.scale.x < 0:
+		wave.rotation = PI
 	
 func take_damage(damage):
 	health -= damage
